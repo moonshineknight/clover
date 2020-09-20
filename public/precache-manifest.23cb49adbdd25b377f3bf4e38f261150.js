87502c1458ed3fe98e4daf49a690d14cadf70b84
moonshineknight <786338929@qq.com>
2020-09-20 23:10:47 +0800

    buid

---
+++ public/precache-manifest.23cb49adbdd25b377f3bf4e38f261150.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d160348b7a898bc21aa576a0675cff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0804c71d7f0ef9d5137",
+    "url": "/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "addf5d121a772f165df0",
+    "url": "/static/css/main.f61d6622.chunk.css"
+  },
+  {
+    "revision": "e0804c71d7f0ef9d5137",
+    "url": "/static/js/2.0aa8f15e.chunk.js"
+  },
+  {
+    "revision": "d3c6009f29fe2fa7cd1af9f0c4f723f4",
+    "url": "/static/js/2.0aa8f15e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "addf5d121a772f165df0",
+    "url": "/static/js/main.ccc99724.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "2cdf1716fbafa2b0f9a00e6116dd99b5",
+    "url": "/static/media/img.2cdf1716.jpeg"
+  }
+]);
